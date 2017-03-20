b0a22d5606bb3a904f88690761a2422de566ac18
Aleksei Ivashkevich <aleksei@ivashkevich.pro>
2017-03-20 14:42:25 +0500

    1) Bad tmp commit

---
+++ test/NumberGuessConsoleApp/NumberGuessActivity.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) e5. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.CoreWf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberGuessConsoleApp
+{
+    class NumberGuessActivity : NativeActivity
+    {
+        InArgument<int> MaxNumber;
+        OutArgument<int> Turns;
+
+
+        int maxNumber;
+        Variable<int> Guess = new Variable<int>("Guess", 0);
+        Variable<int> Target;
+
+        public NumberGuessActivity(int MaxNumber)
+        {
+            maxNumber = MaxNumber;
+            Target = new Variable<int>("Target", new Random().Next(1, maxNumber + 1));
+        }
+
+        protected override void CacheMetadata(NativeActivityMetadata metadata)
+        {
+            metadata.AddImplementationVariable(Guess);
+            metadata.AddImplementationVariable(Target);
+        }
+
+        protected override void Execute(NativeActivityContext context)
+        {
+            context.CreateBookmark("EnterGuess", new BookmarkCallback(BookmarkCallback), BookmarkOptions.MultipleResume);
+        }
+
+        protected override bool CanInduceIdle => true;
+
+        void BookmarkCallback(NativeActivityContext context, Bookmark bookmark, object bookmarkData)
+        {
+            int localGuess, localTarget = Target.Get(context);
+            Console.WriteLine($"Please enter a number between 1 and {maxNumber}");
+            while (!int.TryParse(Console.ReadLine(), out localGuess)) Console.WriteLine("Try again");
+            Guess.Set(context, localGuess);
+            turns.Set(context, turns.Get(context) + 1);
+
+            if (localGuess != localTarget)
+            {
+                if (localGuess < localTarget) Console.WriteLine("Your guess is too low.");
+                else Console.WriteLine("Your guess is too high.");
+            }
+            else context.RemoveBookmark(bookmark.Name);
+        }
+    }
+}
---
+++ test/NumberGuessConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) e5. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.CoreWf;
+using Microsoft.CoreWf.Statements;
+using System;
+
+namespace NumberGuessConsoleApp
+{
+    /// <summary>
+    /// example from https://code.msdn.microsoft.com/windowsapps/Windows-Workflow-deed2cd5 for WF on Core 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = CreateWorkflow();
+            WorkflowApplication
+        }
+
+        static Activity CreateWorkflow()
+        {
+            var workflow = new Sequence();
+            workflow.Activities.Add(new NumberGuessActivity(100));
+            return workflow;
+        }
+    }
+}
